8eb3359d88904bb5886af999b5bbe0c4c100f439
exw <exw@openmailbox.org>
2015-03-19 15:57:23 -0400

    wrote update, deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# move vimrc to home directory, create backup folder
+cp ./.vimrc ~/.vimrc
+mkdir ~/.vim
+mkdir ~/.vim/backup
+
+# move public key list to ~/.ssh
+mkdir ~/.ssh
+cp ./authorized_keys ~/.ssh
+
+# move ubuntu updater script to home directory
+cp ./upd.sh ~
+~/upd.sh
