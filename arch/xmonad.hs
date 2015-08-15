25e820f41ca43adcb33ba6742c6688b15c1117d3
exw <exw@openmailbox.org>
2015-08-14 22:16:45 -0400

    Need copy/paste from gnome terminal

--- arch/xmonad.hs
+++ arch/xmonad.hs
@@ -1,6 +1,7 @@
--- xmonad config used by Vic Fryzel
--- Author: Vic Fryzel
--- http://github.com/vicfryzel/xmonad-config
+-- xmonad config
+-- author: Elliott Wu
+-- http://github.com/exw/config
+-- modified from: Vic Fryzel http://github.com/vicfryzel/xmonad-config
 
 import System.IO
 import System.Exit
@@ -16,6 +17,8 @@ import XMonad.Layout.Tabbed
 import XMonad.Layout.ThreeColumns
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Actions.SpawnOn
+import XMonad.Actions.Volume
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
@@ -76,7 +79,8 @@ myManageHook = composeAll
 -- which denotes layout choice.
 --
 --    Mirror (Tall 1 (3/100) (1/2)) |||
---    Full |||
+-- Google
+-- Full |||
 --    spiral (6/7)) |||
 --    noBorders (fullscreenFull Full)
 --    tabbed shrinkText tabConfig |||
@@ -130,155 +134,167 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   --
 
   -- Start a terminal.  Terminal to start is specified by myTerminal variable.
-  [ ((modMask .|. shiftMask, xK_Return),
-     spawn $ XMonad.terminal conf)
+  [  ((modMask .|. shiftMask, xK_Return),
+      spawn $ XMonad.terminal conf)
+
+  -- Custom Start a terminal Onehanded [2015-08-08 edit]
+     , ((modMask, xK_z),
+        spawn $ XMonad.terminal conf)
 
   -- Lock the screen using xscreensaver.
-  , ((modMask .|. controlMask, xK_l),
-     spawn "xscreensaver-command -lock")
+     , ((modMask .|. shiftMask, xK_l),
+        spawn "xscreensaver-command -lock")
 
   -- Launch dmenu via yeganesh.
   -- Use this to launch programs without a key binding.
-  , ((modMask, xK_p),
-     spawn "dmenu-with-yeganesh")
+     , ((modMask , xK_p),
+        spawn "dmenu-with-yeganesh")
+  
 
   -- Take a screenshot in select mode.
   -- After pressing this key binding, click a window, or draw a rectangle with
   -- the mouse.
-  , ((modMask .|. shiftMask, xK_p),
-     spawn "select-screenshot")
+     , ((modMask .|. shiftMask, xK_p),
+        spawn "select-screenshot")
 
   -- Take full screenshot in multi-head mode.
   -- That is, take a screenshot of everything you see.
-  , ((modMask .|. controlMask .|. shiftMask, xK_p),
-     spawn "screenshot")
+     , ((modMask .|. controlMask .|. shiftMask, xK_p),
+        spawn "screenshot")
 
   -- Fetch a single use password.
-  , ((modMask .|. shiftMask, xK_o),
-     spawn "fetchotp -x")
+     , ((modMask .|. shiftMask, xK_o),
+        spawn "fetchotp -x")
 
 
   -- Mute volume.
-  , ((modMask .|. controlMask, xK_m),
-     spawn "amixer -q set Master toggle")
+     , ((modMask, xK_s),
+  --    spawn "amixer -q set Master toggle")
+  --    toggleMute >> return ())
+        spawn "pactl set-sink-volume 1 0")
 
   -- Decrease volume.
-  , ((modMask .|. controlMask, xK_j),
-     spawn "amixer -q set Master 10%-")
+     , ((modMask, xK_a),
+  --    lowerVolume 4 >> return ()) 
+  --    spawn "amixer -q set Master 10%-")
+        spawn "pactl set-sink-volume 1 24000")
 
   -- Increase volume.
-  , ((modMask .|. controlMask, xK_k),
-     spawn "amixer -q set Master 10%+")
+     , ((modMask , xK_d),
+  --    raiseVolume 4 >> return ())
+  --    spawn "amixer -q set MAster 10%+")
+        spawn "pactl set-sink-volume 1 48000")
 
   -- Audio previous.
-  , ((0, 0x1008FF16),
-     spawn "")
+--  , ((0, 0x1008FF16),
+--     spawn "")
 
   -- Play/pause.
-  , ((0, 0x1008FF14),
-     spawn "")
+--  , ((0, 0x1008FF14),
+--     spawn "")
 
   -- Audio next.
-  , ((0, 0x1008FF17),
-     spawn "")
+--  , ((0, 0x1008FF17),
+ --spawn "")
 
   -- Eject CD tray.
-  , ((0, 0x1008FF2C),
-     spawn "eject -T")
+ -- , ((0, 0x1008FF2C),
+   --  spawn "eject -T")
 
   --------------------------------------------------------------------
   -- "Standard" xmonad key bindings
   --
 
   -- Close focused window.
-  , ((modMask .|. shiftMask, xK_c),
-     kill)
+     , ((modMask .|. shiftMask, xK_c),
+        kill)
 
   -- Cycle through the available layout algorithms.
-  , ((modMask, xK_space),
-     sendMessage NextLayout)
+     , ((modMask, xK_space),
+        sendMessage NextLayout)
 
   --  Reset the layouts on the current workspace to default.
-  , ((modMask .|. shiftMask, xK_space),
-     setLayout $ XMonad.layoutHook conf)
+     , ((modMask .|. shiftMask, xK_space),
+        setLayout $ XMonad.layoutHook conf)
 
   -- Resize viewed windows to the correct size.
-  , ((modMask, xK_n),
-     refresh)
+     , ((modMask, xK_n),
+        refresh)
 
   -- Move focus to the next window.
-  , ((modMask, xK_Tab),
-     windows W.focusDown)
+     , ((modMask, xK_Tab),
+        windows W.focusDown)
 
   -- Move focus to the next window.
-  , ((modMask, xK_j),
-     windows W.focusDown)
+     , ((modMask, xK_j),
+        windows W.focusDown)
 
   -- Move focus to the previous window.
-  , ((modMask, xK_k),
-     windows W.focusUp  )
+     , ((modMask, xK_k),
+        windows W.focusUp  )
 
   -- Move focus to the master window.
-  , ((modMask, xK_m),
-     windows W.focusMaster  )
+     , ((modMask, xK_m),
+        windows W.focusMaster  )
 
   -- Swap the focused window and the master window.
-  , ((modMask, xK_Return),
-     windows W.swapMaster)
+     , ((modMask, xK_Return),
+        windows W.swapMaster)
 
   -- Swap the focused window with the next window.
-  , ((modMask .|. shiftMask, xK_j),
-     windows W.swapDown  )
+     , ((modMask .|. shiftMask, xK_j),
+        windows W.swapDown  )
 
   -- Swap the focused window with the previous window.
-  , ((modMask .|. shiftMask, xK_k),
-     windows W.swapUp    )
+     , ((modMask .|. shiftMask, xK_k),
+        windows W.swapUp    )
 
   -- Shrink the master area.
-  , ((modMask, xK_h),
-     sendMessage Shrink)
+     , ((modMask, xK_h),
+        sendMessage Shrink)
 
   -- Expand the master area.
-  , ((modMask, xK_l),
-     sendMessage Expand)
+     , ((modMask, xK_l),
+        sendMessage Expand)
 
   -- Push window back into tiling.
-  , ((modMask, xK_t),
-     withFocused $ windows . W.sink)
+     , ((modMask, xK_t),
+        withFocused $ windows . W.sink)
 
   -- Increment the number of windows in the master area.
-  , ((modMask, xK_comma),
-     sendMessage (IncMasterN 1))
+     , ((modMask, xK_comma),
+        sendMessage (IncMasterN 1))
 
   -- Decrement the number of windows in the master area.
-  , ((modMask, xK_period),
-     sendMessage (IncMasterN (-1)))
+     , ((modMask, xK_period),
+        sendMessage (IncMasterN (-1)))
 
   -- Toggle the status bar gap.
   -- TODO: update this binding with avoidStruts, ((modMask, xK_b),
 
   -- Quit xmonad.
-  , ((modMask .|. shiftMask, xK_q),
-     io (exitWith ExitSuccess))
+     , ((modMask .|. shiftMask, xK_q),
+        io (exitWith ExitSuccess))
 
   -- Restart xmonad.
-  , ((modMask, xK_q),
-     restart "xmonad" True)
+     , ((modMask, xK_q),
+        restart "xmonad" True)
   ]
+
   ++
 
   -- mod-[1..9], Switch to workspace N
   -- mod-shift-[1..9], Move client to workspace N
-  [((m .|. modMask, k), windows $ f i)
-      | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-      , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+  [  ((m .|. modMask, k), windows $ f i)
+     | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+     , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
   ++
 
   -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
   -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-  [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
-      | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-      , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+   [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+     | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+     , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 
 ------------------------------------------------------------------------
@@ -325,7 +341,8 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
 -- per-workspace layout choices.
 --
 -- By default, do nothing.
-myStartupHook = return ()
+myStartupHook = do
+  spawnOn "web" "terminal"
 
 
 ------------------------------------------------------------------------
@@ -372,3 +389,4 @@ defaults = defaultConfig {
     manageHook         = myManageHook,
     startupHook        = myStartupHook
 }
+
