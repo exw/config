189bf83c34581302c0f04098d01eb275dfda9e9b
exw <ewu120@protonmail.com>
2019-09-29 14:55:23 -0400

    init git aliases and global ignore

--- init.sh
+++ init.sh
@@ -4,14 +4,18 @@
 # see https://github.com/VundleVim/Vundle.vim for .vimrc
 # information based on arcolinux install 19/09/23
 
-# Instructions:
+# Prerequisites before running script:
 #
 # 1. run sudo pacman -R npm and install it from the nvm github
 # 2. follow instructions in YouCompleteMe github
+# 3. get stack and cabal building via ghcup on github/gitlab (optional for now)
 # 3. TODO xmonad stuff
 # 4. TODO ssh key stuff
 
 cp ./.vimrc ~/.vimrc
+cp ./.gitconfig ~/.gitconfig
+cp ./.gitignore_global ~/.gitignore_global
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir ~/.vim/backup
 vim +PluginInstall +qall
